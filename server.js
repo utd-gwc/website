4d3ac3417a88c269614bd8d153a6a90c8bd447df
mustafa <mustafa.sadriwala@utdallas.edu>
2020-10-20 18:14:27 -0500

    additional allowedOrigins for CORS and formatting

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:8081", "https://utd-gwc.github.io/"];
 var corsOptions = {
-    origin: "http://localhost:8081"
+  origin: function (origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
 };
 
 app.use(cors(corsOptions));
@@ -15,11 +27,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to the Girls Who Code website backend!"});
+  res.json({ message: "Welcome to the Girls Who Code website backend!" });
 });
 
 require("./app/routes/event.routes.js")(app);
@@ -28,20 +40,20 @@ require("./app/routes/officer.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
-const db = require("./app/models")
+const db = require("./app/models");
 db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!")
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err)
-        console.log("db.url:: " + db.url);
-        process.exit();
-    })
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    console.log("db.url:: " + db.url);
+    process.exit();
+  });
