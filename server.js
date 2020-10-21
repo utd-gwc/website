955b234cc9f082fc4876fa6a5e09a383dbffe7f6
mustafa <mustafa.sadriwala@utdallas.edu>
2020-10-21 00:45:11 -0500

    once again...

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const app = express();
 const allowedOrigins = ["http://localhost:8081", "https://utd-gwc.github.io/"];
 var corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
+    if (origin == null) {
+      callback(null, true);
+    } else if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
+  },
+};
 
 app.use(cors(corsOptions));
 
